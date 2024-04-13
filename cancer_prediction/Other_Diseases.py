8455ba73e543916d890869309db4d8ad752835fc
Abhinav Omer <111991497+abhinavomer@users.noreply.github.com>
2024-04-14 00:03:34 +0530

    Update Other_Diseases.py

--- cancer_prediction/Other_Diseases.py
+++ cancer_prediction/Other_Diseases.py
@@ -16,15 +16,18 @@ heart_disease_model = pickle.load(open('cancer_prediction/heart_disease_model.h5
 
 parkinsons_model = pickle.load(open('cancer_prediction/parkinsons_model.h5', 'rb'))
 
+kidney_model=pickle.load(open('cancer_prediction/kidney_detector.pickle', 'rb'))
+
 # sidebar for navigation
 with st.sidebar:
     selected = option_menu('Multiple Disease Prediction System',
 
                            ['Diabetes Prediction',
                             'Heart Disease Prediction',
-                            'Parkinsons Prediction'],
+                            'Parkinsons Prediction',
+                           'Chronic Kidney Disease']],
                            menu_icon='hospital-fill',
-                           icons=['activity', 'heart', 'person'],
+                           icons=['activity', 'heart', 'person','meta'],
                            default_index=0)
 
 
@@ -244,3 +247,107 @@ if selected == "Parkinsons Prediction":
             parkinsons_diagnosis = "The person does not have Parkinson's disease"
 
     st.success(parkinsons_diagnosis)
+# Kidney disease's Prediction Page
+if selected == "Chronic Kidney Disease":
+
+    # page title
+    st.title("Chronic Kidney Disease Prediction using ML")
+
+    col1, col2, col3, col4, col5 = st.columns(5)
+
+    with col1:
+        age = st.text_input('Age')
+
+    with col2:
+        blood_pressure = st.text_input('Blood Pressure')
+
+    with col3:
+        specific_gravity = st.text_input('Specific Gravity')
+
+    with col4:
+        albumin = st.text_input('Albumin')
+
+    with col5:
+        sugar = st.text_input('Sugar')
+
+    with col1:
+        red_blood_cells = st.text_input('Red Blood Cells')
+
+    with col2:
+        pus_cell = st.text_input('Pus Cell')
+
+    with col3:
+        pus_cell_clumps = st.text_input('Pus Cell Clumps')
+
+    with col4:
+        bacteria = st.text_input('Bacteria')
+
+    with col5:
+        blood_glucose_random = st.text_input('Blood Glucose Random')
+
+    with col1:
+        blood_urea = st.text_input('Blood Urea')
+
+    with col2:
+        serum_creatinine = st.text_input('Serum Creatinine')
+
+    with col3:
+        sodium = st.text_input('Sodium')
+
+    with col4:
+        potassium = st.text_input('Potassium')
+
+    with col5:
+        haemoglobin = st.text_input('Haemoglobin')
+
+    with col1:
+        packed_cell_volume = st.text_input('Packed Cell Volume')
+
+    with col2:
+        white_blood_cell_count = st.text_input('White Blood Cell Count')
+
+    with col3:
+        red_blood_cell_count = st.text_input('Red Blood Cell Count')
+
+    with col4:
+        hypertension = st.text_input('Hypertension')
+
+    with col5:
+        diabetes_mellitus = st.text_input('Diabetes Mellitus')
+
+    with col1:
+        coronary_artery_disease = st.text_input('Coronary Artery Disease')
+
+    with col2:
+        appetite = st.text_input('Appetite')
+
+    with col3:
+        peda_edema = st.text_input('Peda Edema')
+
+    with col4:
+        aanemia = st.text_input('Aanemia')  
+
+    # code for Prediction
+    kidney_diagnosis = ''
+
+    # creating a button for Prediction    
+    if st.button("Kidney's Test Result"):
+
+        user_input = [age, blood_pressure, specific_gravity, albumin, sugar,
+              red_blood_cells, pus_cell, pus_cell_clumps, bacteria,
+              blood_glucose_random, blood_urea, serum_creatinine, sodium,
+              potassium, haemoglobin, packed_cell_volume,
+              white_blood_cell_count, red_blood_cell_count, hypertension,
+              diabetes_mellitus, coronary_artery_disease, appetite,
+              peda_edema, aanemia]
+
+        user_input = [float(x) for x in user_input]
+
+        kidney_prediction = kidney_model.predict([user_input])
+
+        if kidney_prediction[0] == 0:
+            kidney_diagnosis = "The person has Kidney disease"
+        else:
+            kidney_diagnosis = "The person does not have Kidney disease"
+
+    st.success(kidney_diagnosis)
